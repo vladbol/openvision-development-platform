e5775ea01be60359e7e796b58d1a0fcd8a366680
Persian Prince <persianpros@yahoo.com>
2019-06-25 16:21:44 +0430

    Update image.sh

--- image.sh
+++ image.sh
@@ -868,4 +868,4 @@ then
 fi
 echo -e ""
 echo -e "${RED}Done, the compiled image is in ${NC}build/tmp/deploy/images/${GREEN}$MACHINE$MACHINESPECIFIC ${RED}folder!"
-echo -e "It's a zipped file like ${NC}openvision-enigma2-develop-9.1-rx-${GREEN}$MACHINE$MACHINESPECIFIC${NC}.zip"
+echo -e "It's a zipped file like ${NC}openvision-enigma2-$DISTRO_VERSION-9.1-rx-${GREEN}$MACHINE$MACHINESPECIFIC${NC}.zip"
