100d0ce8423c1be33b918c274356b4963f13af58
Persian Prince <persianpros@yahoo.com>
2019-06-26 01:29:48 +0430

    Update ltsubuntu.sh

--- ltsubuntu.sh
+++ ltsubuntu.sh
@@ -10,6 +10,6 @@ echo ""
 sudo apt-get update
 echo "Now install what we need, answer yes and wait for complete download"
 echo ""
-sudo apt-get install autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git gzip help2man ncurses-bin libncurses5-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget chrpath libxml2-utils xsltproc python-setuptools libc6 genromfs mtd-utils dpkg-dev sshpass poedit translate-toolkit xclip linux-firmware linux-headers-`uname -r` linux-headers-generic linux-image-generic linux-libc-dev linux-source u-boot-tools upx-ucl doxygen repo optipng python-dev libglib2.0-dev pngquant default-jdk android-tools-fastboot android-tools-fsutils libssl-dev
+sudo apt-get install -y autoconf automake bison bzip2 cvs diffstat flex g++ gawk gcc gettext git gzip help2man ncurses-bin libncurses5-dev libc6-dev libtool make texinfo patch perl pkg-config subversion tar texi2html wget chrpath libxml2-utils xsltproc python-setuptools libc6 genromfs mtd-utils dpkg-dev sshpass poedit translate-toolkit xclip linux-firmware linux-headers-`uname -r` linux-headers-generic linux-image-generic linux-libc-dev linux-source u-boot-tools upx-ucl doxygen repo optipng python-dev libglib2.0-dev pngquant default-jdk android-tools-fastboot android-tools-fsutils libssl-dev
 echo ""
 echo "Done, now you can compile an Open Vision image!"
